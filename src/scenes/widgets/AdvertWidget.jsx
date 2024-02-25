7aa9f55c4a46f2a56f5421931ca112ca331c914a
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-02-25 21:58:46 +0530

    check

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -14,7 +14,7 @@ const AdvertWidget = () => {
         <Typography color={dark} variant="h5" fontWeight="500">
           Sponsored
         </Typography>
-        <Typography color={medium}>Create Ad</Typography>
+        <Typography color={medium}>Close Ad X</Typography>
       </FlexBetween>
       <img
         width="100%"
@@ -24,12 +24,10 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>The Chennai Silks</Typography>
       </FlexBetween>
-      <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        <Typography color={medium} m="0.5rem 0">
+       Let the Celebration Begins
       </Typography>
     </WidgetWrapper>
   );
